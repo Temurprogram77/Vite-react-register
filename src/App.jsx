d83fd492781f28e5bbf4233b516ef3155c0a53ae
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-15 17:45:28 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,32 @@ const App = () => {
         <h2>Ro'yxatdan o'tish</h2>
         <input type="text" placeholder="Foydalanuvchi nomi" name="username" />
         <input type="email" placeholder="Email" name="email" />
-        <input type="password" placeholder="Parol" name="password" />
-        <input
-          type="password"
-          placeholder="Parolni qayta kiriting"
-          name="confirm-password"
-        />
+        <div class="input-group">
+          <input
+            type="password"
+            placeholder="Parol"
+            name="password"
+            id="password"
+          />
+          <span class="eye-icon" onclick="togglePassword('password', this)">
+            👁️
+          </span>
+        </div>
+
+        <div class="input-group">
+          <input
+            type="password"
+            placeholder="Parolni qayta kiriting"
+            name="confirm-password"
+            id="confirm-password"
+          />
+          <span
+            class="eye-icon"
+            onclick="togglePassword('confirm-password', this)"
+          >
+            👁️
+          </span>
+        </div>
         <button onclick="register()">Ro'yxatdan o'tish</button>
       </div>
     </div>
