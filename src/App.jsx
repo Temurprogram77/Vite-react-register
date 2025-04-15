479e9afa1bb6d57dce87007568cae1b58f6dd411
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-15 17:41:26 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div className="register-box">
+        <h2>Ro'yxatdan o'tish</h2>
+        <input type="text" placeholder="Foydalanuvchi nomi" name="username" />
+        <input type="email" placeholder="Email" name="email" />
+        <input type="password" placeholder="Parol" name="password" />
+        <input
+          type="password"
+          placeholder="Parolni qayta kiriting"
+          name="confirm-password"
+        />
+        <button onclick="register()">Ro'yxatdan o'tish</button>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
